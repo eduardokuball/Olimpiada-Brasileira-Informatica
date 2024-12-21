6b467677fa9bc8f1e54ba478249779c06d1eca6e
Eduardo <eduardokuball@gmail.com>
2024-12-21 11:34:55 -0300

    add

--- OBI/1999/segunda fase/C.js
+++ OBI/1999/segunda fase/C.js
@@ -1,46 +1,65 @@
-Array.prototype.normalizeIndex = function(i) {
-    const normalized = i % this.length;
-    console.log(normalized);
-    
-    return normalized;
-};
+let [quantity, favored] = prompt('digite a quantidade de jogadores e o jogador favorecido')
+.split(' ')
+.map(Number);
 
-Array.prototype.incrementRight = function(i, x=1) {
-    this[this.normalizeIndex(i)] -= x;
-    this[this.normalizeIndex(i+1)] += x;
-    console.log(this);
-};
+let obj = [];
 
-const players = parseInt(prompt());
-const favPlayer = parseInt(prompt());
-
-const buzios = Array(players).fill(1) || [];
-buzios[favPlayer-1]++;
-
-let turns = 0;
-
-// [1, 2, 3, 4, 5]
-const playersInGame = Array.from({ length: players }, (_, i) => i + 1);
-
-function buziosLoop() {
-    for (let i = 0; i < buzios.length; i++) {
-        const playerBuzios = buzios[i];
-        const nextPlayerBuzios = buzios[i+1];
-
-        if (playerBuzios <= 0) continue;
-        if (nextPlayerBuzios <= 0) continue;
-        
-        if ((turns+1) % 2 === 0) {
-            buzios.incrementRight(i, 2);
-            turns++;
-            continue;
-        }
-
-        buzios.incrementRight(i);
-        turns++;
+for (let i = 0; i < quantity; i++) {
+    if (i + 1 === favored) {
+        obj.push({
+            buzios: 2,
+            position: i + 1,
+        });
+    } else {
+        obj.push({
+            buzios: 1,
+            position: i + 1,
+        });
     }
 }
 
-buziosLoop();
+function isWinner() {
+    let count = 0;
+    obj.forEach((item) => {
+        if (item.buzios > 0) {
+            count++;
+        }
+    });
+    return count === 1;
+}
 
-console.log(buzios);
+let rounds = 0;
+let turns = 1;
+
+while (obj.length > 1) {
+    let currentIndex = rounds % obj.length;
+    let nextIndex = (currentIndex + 1) % obj.length; 
+    if (turns % 2 !== 0) { 
+        obj[currentIndex].buzios -= 1;
+        obj[nextIndex].buzios += 1;
+        turns++;
+        rounds++;
+    } else {
+        obj[currentIndex].buzios -= 2;
+        obj[nextIndex].buzios += 2;
+        turns++;
+        rounds++;
+    }
+
+
+    if (obj[currentIndex].buzios <= 0) {
+        obj.splice(currentIndex, 1);
+        rounds = currentIndex;
+    }
+};
+
+let winner = obj.map(item => item.position).join(' ');
+
+
+console.log(`turnos: ${turns - 1}`);
+console.log(`vencedor: ${winner}`);
+
+
+
+
+
