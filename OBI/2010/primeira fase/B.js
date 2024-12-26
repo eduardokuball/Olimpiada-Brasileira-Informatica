f85a8a1288df9fe9765c9c4ca58546250fc00216
Eduardo <eduardokuball@gmail.com>
2024-12-26 15:42:48 -0300

    adicionando a questão B da primeira fase de 2010

---
+++ OBI/2010/primeira fase/B.js
@@ -0,0 +1,24 @@
+const [lines,columns] = prompt("linhas e colunas")
+.split(" ")
+.map(Number);
+
+const field = [];
+
+for (let i = 0; i < lines; i++) {
+    const row = prompt(`digite a ${i + 1} linha`)
+    .split("");
+    field.push(row);
+};
+
+const commands = Number(prompt('quantos comandos?'));
+let shipsHit = 0;
+for(let i = 0; i < commands; i++){
+    const [x,y] = prompt('coordenada do comando')
+    .split(' ')
+    .map(Number);
+    if(field[x - 1][y - 1] === '#'){
+        shipsHit++;
+    }
+}
+
+console.log(shipsHit);
