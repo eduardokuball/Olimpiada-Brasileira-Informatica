1b06c9c18be45d06b3afa054cbd4596a7e493784
Eduardo <eduardokuball@gmail.com>
2025-01-09 16:41:38 -0300

    Adicionando a questão B da primeira fase de 2014

---
+++ OBI/2014/primeira fase/B.js
@@ -0,0 +1,18 @@
+function decrypt(string){   
+    let message = string;
+    let result = "";
+    for(let i = 0; i < message.length; i++){
+        if(message[i] == 'p'){
+            if(message[i + 1] == 'p'){
+                result += 'p'
+            }
+        } else {
+            result += message[i];
+        }
+    } 
+    return result.replaceAll('pp', 'p');
+};
+
+const string = prompt();
+
+console.log(decrypt('pppp'));
