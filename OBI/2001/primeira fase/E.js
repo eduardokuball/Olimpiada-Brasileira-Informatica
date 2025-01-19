6206b5394ba2c4aef6bb0009f8d57a77baca9ae4
Eduardo <eduardokuball@gmail.com>
2025-01-19 15:51:07 -0300

    adicionando a questão E da primeira fase de 2001

---
+++ OBI/2001/primeira fase/E.js
@@ -0,0 +1,33 @@
+function calculate(expressao) {
+    let resultado = 0;
+    let numeroAtual = '';
+    let operador = '+';
+
+    for (let i = 0; i < expressao.length; i++) {
+        const char = expressao[i];
+
+        if (!isNaN(char)) {
+            numeroAtual += char;
+        }
+
+        if (isNaN(char) || i === expressao.length - 1) {
+            if (operador === '+') {
+                resultado += parseInt(numeroAtual, 10);
+            } else if (operador === '-') {
+                resultado -= parseInt(numeroAtual, 10);
+            }
+
+            operador = char;
+            numeroAtual = '';
+        }
+    }
+    
+        return resultado;
+}
+
+const quantity = parseInt(prompt());
+const expression = prompt()
+    .split('');
+
+
+console.log(calculate(expression));
