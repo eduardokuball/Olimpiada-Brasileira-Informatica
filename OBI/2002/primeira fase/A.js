68d2bd6851e0eab9ae14fb833f98e760eea8babc
Eduardo <eduardokuball@gmail.com>
2025-01-19 17:42:19 -0300

    adicionando a questão A da primeira fase de 2002

---
+++ OBI/2002/primeira fase/A.js
@@ -0,0 +1,28 @@
+function getTemperatures(meditions){
+    let temperatures = [];
+    for(let i = 0; i < meditions; i++){
+        temperatures.push(parseInt(prompt()));
+    }
+    return temperatures;
+};
+function averages(temperatures){
+    let averages = [];
+    for(let i = 0; i < temperatures.length - (interval - 1); i++){
+        const analising = temperatures.slice(i,i + interval);
+        const sum = analising.reduce((acc, curr) => acc + curr, 0);
+        const average = sum / analising.length;
+        averages.push(Math.trunc(average));
+    };
+    averages.sort((a,b) => a - b);
+    return [Math.min(...averages), Math.max(...averages)].join(' '); 
+}
+
+const [meditions, interval] = prompt()
+    .split(' ')
+    .map(e => parseInt(e));
+
+const temperatures = getTemperatures(meditions);
+
+const averagesTemperatures = averages(temperatures);
+
+console.log(averagesTemperatures);
