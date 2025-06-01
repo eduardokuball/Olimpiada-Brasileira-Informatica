2dd5e6b8d456d0672d389aa50f8646d2fc030afa
Eduardo <eduardokuball@gmail.com>
2025-06-01 10:57:28 -0300

    adicionando a questão C da segunda fase de 2009

--- OBI/2009/segunda fase/C.js
+++ OBI/2009/segunda fase/C.js
@@ -1,63 +1,74 @@
-const [lines, c] = prompt()
+const [lines, columns] = prompt()
     .split(' ')
     .map((e) => {
         return parseInt(e);
     });
 
-const auditorio = [];
+const auditorium = [];
 
 for (let i = 0; i < lines; i++) {
-    const cadeiras = prompt()
+    const chairs = prompt()
     .split(' ')
     .map((e) => {
         return parseInt(e);
     });
-    auditorio.push(cadeiras);
+    auditorium.push(chairs);
 }
 
-const markCol = Array(c).fill(0);
-const col = Array.from({ length: c }, (_, i) => (auditorio[0][i] - 1) % c);
+const markCol = Array(columns).fill(0);
+const col = Array.from({ length: columns }, (_, i) => (auditorium[0][i] - 1) % columns);
 
 let nc = 0;
 const mc = [];
 
-for (let i = 0; i < c; i++) {
+for (let i = 0; i < columns; i++) {
     if (markCol[i]) continue;
-    let atual = col[i];
+    let current = col[i];
 
-    while (atual !== i) {
-        const trocaColuna = [atual + 1, col[atual] + 1];
-        mc.push(trocaColuna);
+    while (current !== i) {
+        const switchColumn = [current + 1, col[current] + 1];
+        mc.push(switchColumn);
         nc++;
-        markCol[atual] = 1;
-        atual = col[atual];
+        markCol[current] = 1;
+        current = col[current];
     }
 }
 
-const markLin = Array(l).fill(0);
-const lin = Array.from({ length: lines }, (_, i) => Math.floor((auditorio[i][0] - 1) / c));
+const markLin = Array(lines).fill(0);
+const lin = Array.from({ length: lines }, (_, i) => Math.floor((auditorium[i][0] - 1) / columns));
 
 let nl = 0;
 const ml = [];
 
 for (let i = 0; i < lines; i++) {
     if (markLin[i]) continue;
-    let atual = lin[i];
+    let current = lin[i];
 
-    while (atual !== i) {
-        const trocaLinha = [atual + 1, lin[atual] + 1];
-        ml.push(trocaLinha);
+    while (current !== i) {
+        const switchLine = [current + 1, lin[current] + 1];
+        ml.push(switchLine);
         nl++;
-        markLin[atual] = 1;
-        atual = lin[atual];
+        markLin[current] = 1;
+        current = lin[current];
     }
 }
 
+ml.flat().sort((a, b) => b - a);
+mc.flat().sort((a, b) => b - a);
+
 console.log(nc + nl);
-for (const [a, b] of ml) {
-    console.log(a, b);
+for (let [a, b] of ml) {
+    if(a > b){
+        [a, b] = [b, a];
+    }
+    const output = `L ${a} ${b}`;
+    console.log(output);
 }
-for (const [a, b] of mc) {
-    console.log(a, b);
+for (let [a, b] of mc) {
+    if(a > b){
+        [a, b] = [b, a];
+    }
+    const output = `C ${a} ${b}`;
+    console.log(output);
 }
 
