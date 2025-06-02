747cfc5f700ed7379fb384b0d5d763240cb76569
Eduardo <eduardokuball@gmail.com>
2025-06-02 09:26:44 -0300

    adicionando a questão A da primeira fase de 2019

---
+++ OBI/2019/primeira fase/A.js
@@ -0,0 +1,29 @@
+const N = parseInt(prompt());
+const v = [];
+
+for (let i = 0; i < N; i++) {
+    v.push(parseInt(prompt()));
+}
+
+let res = 1;
+const seen = new Set();
+
+for (let i = 0; i < N; i++) {
+    if (seen.has(v[i])) continue;
+    seen.add(v[i]);
+
+    for (let j = 0; j < N; j++) {
+        let current = 0;
+        let last = -1;
+
+        for (let k = 0; k < N; k++) {
+            if ((v[k] === v[i] || v[k] === v[j]) && v[k] !== last) {
+                current++;
+                last = v[k];
+            }
+        }
+        res = Math.max(res, current);
+    }
+}
+
+console.log(res);
