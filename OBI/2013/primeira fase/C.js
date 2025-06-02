113c236bc2569935ad5a87298e58bbd273f55f66
Eduardo <eduardokuball@gmail.com>
2025-06-02 09:36:26 -0300

    adicionando a questão C da primeira fase de 2013

---
+++ OBI/2013/primeira fase/C.js
@@ -0,0 +1,85 @@
+function findTranslation(word) {
+    if (word in dictionary) {
+        return dictionary[word];
+    } else {
+        folderSizes.push(word.length + 1);
+        const position = Object.keys(dictionary).length;
+        dictionary[word] = position;
+        return position;
+    }
+}
+
+function splitPath(path) {
+    let start = 0;
+    const result = [0];
+
+    for (let i = 0; i < path.length; i++) {
+        if (path[i] === '/') {
+            const folder = path.substring(start, i);
+            const folderIndex = findTranslation(folder);
+            result.push(folderIndex);
+            start = i + 1;
+        }
+    }
+
+    return result;
+}
+
+function countFilesRecursively(index) {
+    for (const neighbor of adjacency[index]) {
+        if (visited[neighbor]) continue;
+        countFilesRecursively(neighbor);
+        visited[neighbor] = true;
+        filesInSubfolders[index] += filesInSubfolders[neighbor];
+    }
+}
+
+function searchBestSolution(index, currentSolution) {
+    for (const neighbor of adjacency[index]) {
+        if (visited[neighbor]) continue;
+
+        const newSolution = currentSolution 
+            - (folderSizes[neighbor] * filesInSubfolders[neighbor])
+            + (3 * (n - filesInSubfolders[neighbor]));
+
+        bestSolution = Math.min(bestSolution, newSolution);
+
+        searchBestSolution(neighbor, newSolution);
+        visited[neighbor] = true;
+    }
+}
+
+let n = parseInt(prompt());
+
+const dictionary = { "": 0 };
+const folderSizes = [0];
+const filesInSubfolders = Array(100000).fill(0);
+const adjacency = Array.from({ length: 100000 }, () => []);
+
+let initialSize = 0;
+
+for (let i = 0; i < n; i++) {
+    const filePath = prompt().trim();
+    initialSize += filePath.length;
+
+    const pathIndices = splitPath(filePath);
+    filesInSubfolders[pathIndices[pathIndices.length - 1]]++;
+
+    for (let j = 0; j < pathIndices.length - 1; j++) {
+        const u = pathIndices[j];
+        const v = pathIndices[j + 1];
+        adjacency[u].push(v);
+    }
+}
+
+let visited = Array(100000).fill(false);
+countFilesRecursively(0);
+
+visited = Array(100000).fill(false);
+let bestSolution = initialSize;
+searchBestSolution(0, initialSize);
+
+console.log(bestSolution);
+
+
+
